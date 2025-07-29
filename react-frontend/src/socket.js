095340cc5842d7fabad3e52f68d2f2c99ef56bd6
sharma-nishant-1140 <ntigersharma231@gmail.com>
2025-07-29 18:37:50 +0530

    Minor changes

--- react-frontend/src/socket.js
+++ react-frontend/src/socket.js
@@ -1,4 +1,7 @@
 import { io } from "socket.io-client";
+const URL = import.meta.env.PROD
+  ? "wss://chatmate.onrender.com"
+  : "http://localhost:5000";
 
 export const createsocket = (name, room) => {
   return io("http://localhost:5000", {
