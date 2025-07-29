5c8dd88734a4bb0b43780e11f62489d44c6d5042
sharma-nishant-1140 <ntigersharma231@gmail.com>
2025-07-29 19:43:12 +0530

    Added wake up ping logic

--- react-frontend/src/socket.js
+++ react-frontend/src/socket.js
@@ -3,7 +3,16 @@ const URL = import.meta.env.PROD
   ? "https://chatmate-fsag.onrender.com/"
   : "http://localhost:5000";
 
-export const createsocket = (name, room) => {
+export const createsocket = async (name, room) => {
+
+  if (import.meta.env.PROD) {
+    try {
+      await fetch(`${URL}/`);
+    } catch (err) {
+      console.warn("Wake-up ping failed:", err);
+    }
+  }
+
   return io(URL, {
     transports: ["websocket", "polling"],
     auth: { name, room },
