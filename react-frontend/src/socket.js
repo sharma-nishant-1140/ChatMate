2ce95da53bc3f3f8a06239cee01ca131b4dfddce
sharma-nishant-1140 <ntigersharma231@gmail.com>
2025-07-29 19:48:52 +0530

    Minor changes

--- react-frontend/src/socket.js
+++ react-frontend/src/socket.js
@@ -3,16 +3,7 @@ const URL = import.meta.env.PROD
   ? "https://chatmate-fsag.onrender.com/"
   : "http://localhost:5000";
 
-export const createsocket = async (name, room) => {
-
-  if (import.meta.env.PROD) {
-    try {
-      await fetch(`${URL}/`);
-    } catch (err) {
-      console.warn("Wake-up ping failed:", err);
-    }
-  }
-
+export const createsocket = (name, room) => {
   return io(URL, {
     transports: ["websocket", "polling"],
     auth: { name, room },
